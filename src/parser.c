e6fc50bd68d2a8071a76a0bef6ac1cd930bfc4ba
AndreasKarampasis <ankarampasis@gmail.com>
2026-06-12 16:42:54 +0300

    added support for immediate and branch instructions

--- src/parser.c
+++ src/parser.c
@@ -1,33 +1,12 @@
 #include "parser.h"
 
+#include <assert.h>
 #include <stdio.h>
 
 #include "isa.h"
-// TODO: move it to a separate header/module
-typedef struct {
-  bool is_valid;
-  uint8_t opcode;
-  uint8_t rd;
-  uint8_t funct3;
-  uint8_t rs1;
-  uint8_t rs2;
-  uint8_t funct7;
-} instruction_t;
 
-// static void synchronise(parser_t* parser);
-static void error_at(parser_t* parser, const char* message);
-static void parser_advance(parser_t* parser);
-static bool parser_check(parser_t* parser, tokentype_t type);
-static bool parser_match(parser_t* parser, tokentype_t type);
-static bool parser_expect(parser_t* parser, tokentype_t type,
-                          const char* error_message);
-static uint32_t parse_inst(parser_t* parser);
-static uint32_t parse_r_type(parser_t* parser, const isa_entry_t* inst);
-static uint32_t parse_i_type(parser_t* parser, const isa_entry_t* inst);
-static uint32_t parse_s_type(parser_t* parser, const isa_entry_t* inst);
 static uint32_t parse_b_type(parser_t* parser, const isa_entry_t* inst);
 static uint32_t parse_u_type(parser_t* parser, const isa_entry_t* inst);
-static uint32_t parse_j_type(parser_t* parser, const isa_entry_t* inst);
 static uint8_t parse_reg(token_t token);
 static int parse_imm(token_t token);
 
@@ -83,17 +62,9 @@ static bool parser_expect(parser_t* parser, tokentype_t type,
   return false;
 }
 
-static uint8_t parse_reg(token_t token) {
-  printf("Parsed register: %.*s (id=%u)\n", (int)token.length, token.start,
-         token.reg_id);
-  return token.reg_id;
-}
+static uint8_t parse_reg(token_t token) { return token.reg_id; }
 
-static int parse_imm(token_t token) {
-  printf("Parsed immediate: %.*s (value=%d)\n", (int)token.length, token.start,
-         token.int_value);
-  return token.int_value;
-}
+static int parse_imm(token_t token) { return token.int_value; }
 
 static uint32_t parse_s_type(parser_t* parser, const isa_entry_t* inst) {
   parser_expect(parser, TOK_REGISTER, "Expected register");
@@ -116,11 +87,36 @@ static uint32_t parse_s_type(parser_t* parser, const isa_entry_t* inst) {
   instruction |= (rs1 & 0x1F) << 15;           // rs1 in bits 19:15
   instruction |= (rs2 & 0x1F) << 20;           // rs2 in bits 24:20
   instruction |= ((imm >> 5) & 0x7F) << 25;    // imm[11:5] in bits 31:25
+  printf("%08X\n", instruction);
 
   return instruction;
 }
 
-static uint32_t parse_i_type(parser_t* parser, const isa_entry_t* inst) {
+static uint32_t parse_jalr(parser_t* parser, const isa_entry_t* inst) {
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rd = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_COMMA, "Expected comma");
+  parser_expect(parser, TOK_INTEGER, "Expected immediate value");
+  int imm = parse_imm(parser->previous);
+
+  parser_expect(parser, TOK_LPAREN, "Expected '('");
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rs1 = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_RPAREN, "Expected ')'");
+  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
+
+  uint32_t instruction = inst->opcode;
+  instruction |= (rd & 0x1F) << 7;
+  instruction |= (inst->funct3 & 0x07) << 12;
+  instruction |= (rs1 & 0x1F) << 15;
+  instruction |= (imm & 0xFFF) << 20;
+  printf("%08X\n", instruction);
+  return instruction;
+}
+
+static uint32_t parse_alu_imm(parser_t* parser, const isa_entry_t* inst) {
   parser_expect(parser, TOK_REGISTER, "Expected register");
   uint8_t rd = parse_reg(parser->previous);
 
@@ -130,15 +126,101 @@ static uint32_t parse_i_type(parser_t* parser, const isa_entry_t* inst) {
 
   parser_expect(parser, TOK_COMMA, "Expected comma");
   parser_expect(parser, TOK_INTEGER, "Expected immediate value");
-  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
   int imm = parse_imm(parser->previous);
+
+  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
 
   uint32_t instruction = inst->opcode;
   instruction |= (rd & 0x1F) << 7;
   instruction |= (inst->funct3 & 0x07) << 12;
   instruction |= (rs1 & 0x1F) << 15;
   instruction |= (imm & 0xFFF) << 20;
+  printf("%08X\n", instruction);
   return instruction;
+}
+
+static uint32_t parse_load(parser_t* parser, const isa_entry_t* inst) {
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rd = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_COMMA, "Expected comma");
+  parser_expect(parser, TOK_INTEGER, "Expected immediate value");
+  int imm = parse_imm(parser->previous);
+
+  parser_expect(parser, TOK_LPAREN, "Expected '('");
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rs1 = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_RPAREN, "Expected ')'");
+  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
+
+  uint32_t instruction = inst->opcode;
+  instruction |= (rd & 0x1F) << 7;
+  instruction |= (inst->funct3 & 0x07) << 12;
+  instruction |= (rs1 & 0x1F) << 15;
+  instruction |= (imm & 0xFFF) << 20;
+  printf("%08X\n", instruction);
+  return instruction;
+}
+
+static uint32_t parse_b_type(parser_t* parser, const isa_entry_t* inst) {
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rs1 = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_COMMA, "Expected comma");
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rs2 = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_COMMA, "Expected comma");
+  parser_expect(parser, TOK_INTEGER, "Expected immediate value");
+  int imm = parse_imm(parser->previous);
+  // labels?
+
+  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
+
+  uint32_t instruction = inst->opcode;
+  instruction |= (inst->funct3 & 0x07) << 12;  // funct3 in bits 14:12
+  instruction |= (rs1 & 0x1F) << 15;           // rs1 in bits 19:15
+  instruction |= (rs2 & 0x1F) << 20;           // rs2 in bits 24:20
+  instruction |= ((imm >> 5) & 0x7E) << 25;    // imm[11:5] in bits 31:25
+  instruction |= ((imm & 0x1F) << 7);          // imm[4:0] in bits 11:7
+  printf("%08X\n", instruction);
+  return instruction;
+}
+
+static uint32_t parser_j_type(parser_t* parser, const isa_entry_t* inst) {
+  parser_expect(parser, TOK_REGISTER, "Expected register");
+  uint8_t rd = parse_reg(parser->previous);
+
+  parser_expect(parser, TOK_COMMA, "Expected comma");
+  parser_expect(parser, TOK_INTEGER, "Expected immediate value");
+  int imm = parse_imm(parser->previous);
+
+  parser_expect(parser, TOK_NEWLINE, "expected newline after instruction");
+
+  uint32_t instruction = inst->opcode;
+  instruction |= (rd & 0x1F) << 7;
+  instruction |= (imm & 0xFFFFF) << 12;  // imm[19:0] in bits 31:12
+  printf("%08X\n", instruction);
+  return instruction;
+}
+
+#define OPCODE_JALR 0x67
+#define OPCODE_LOAD 0x03
+#define OPCODE_ALU_IMM 0x13
+static uint32_t parse_i_type(parser_t* parser, const isa_entry_t* inst) {
+  switch (inst->opcode) {
+    case OPCODE_ALU_IMM:
+      return parse_alu_imm(parser, inst);
+    case 0x73:  // ECALL/EBREAK
+      break;
+    case OPCODE_JALR:
+      return parse_jalr(parser, inst);
+    case OPCODE_LOAD:
+      return parse_load(parser, inst);
+  }
+  error_at(parser, "Unsupported I-type instruction");
+  return 0xFFFFFFFF;  // Invalid instruction encoding
 }
 
 static uint32_t parse_r_type(parser_t* parser, const isa_entry_t* inst) {
@@ -160,6 +242,7 @@ static uint32_t parse_r_type(parser_t* parser, const isa_entry_t* inst) {
   instruction |= (rs1 & 0x1F) << 15;
   instruction |= (rs2 & 0x1F) << 20;
   instruction |= (inst->funct7 & 0x7F) << 25;
+  printf("%08X\n", instruction);
   return instruction;
 }
 
@@ -192,11 +275,11 @@ static uint32_t parse_inst(parser_t* parser) {
     case S_TYPE:
       return parse_s_type(parser, inst);
     case B_TYPE:
-      assert(0 && "B-type parsing not implemented yet");
+      return parse_b_type(parser, inst);
     case U_TYPE:
       assert(0 && "U-type parsing not implemented yet");
     case J_TYPE:
-      assert(0 && "J-type parsing not implemented yet");
+      return parser_j_type(parser, inst);
     default:
       error_at(parser, "Invalid instruction format");
   }
@@ -211,8 +294,9 @@ void parser_init(parser_t* parser, lexer_t* lexer) {
 
 void parser_parse(parser_t* parser) {
   while (!parser_check(parser, TOK_EOF)) {
-    if (parser_match(parser, TOK_NEWLINE)) continue;
-    if (parser_check(parser, TOK_INSTRUCTION)) {
+    if (parser_match(parser, TOK_NEWLINE)) {
+      continue;
+    } else if (parser_check(parser, TOK_INSTRUCTION)) {
       parse_inst(parser);
     } else if (parser_check(parser, TOK_NAME)) {
       parse_label(parser);
--- tests/i_type.asm
+++ tests/i_type.asm
@@ -1,1 +1,4 @@
 addi x1, x2, 3
+slti  x1, x2, 100
+lb   x1, 0(x2)
+jalr x1, 0(x2)
